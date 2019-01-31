c73ac65c6efcb51bbec61f891c83c8e07680cdca
Samyok Nepal <nepalsamyok@gmail.com>
2019-01-31 17:16:20 -0600

    Add files via upload

--- static/js/search.js
+++ static/js/search.js
@@ -1,12 +1,5 @@
 $(document).ready(function () {
-    let scrolled = false;
     $("#searchBar").on("keyup", function () {
-        if(!scrolled){
-            $([document.documentElement, document.body]).animate({
-                scrollTop: $("#searchBar").offset().top
-            }, 500);
-            scrolled = true;
-        }
         let value = $(this).val().toLowerCase();
         $("tbody tr").filter(function () {
             $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
