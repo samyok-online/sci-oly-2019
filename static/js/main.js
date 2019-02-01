05d4e92d44833be03c35dea4cc01e459a3dcb014
nepaltechguy2 <nepalsamyok@gmail.com>
2019-01-31 22:32:05 -0600

    Updated website, will think about more ways to do events.js later

--- static/js/main.js
+++ static/js/main.js
@@ -16,16 +16,16 @@ $(document).ready(()=>{
         }
     });
     let string = "Anatomy and Physiology  | Helen Zhang and Angela LansangXAstronomy | Bindi Kasu and Serena An/Tristina TingXCircuit Lab  | Grant Sternhagen *Can have one more personXCodebusters | Prasoon Khare, Samyok Nepal, and Carissa WittXDesigner Genes | Samyok Nepal and Carissa WittXDisease Detectives  | Marina and Jane AdryXDynamic Planet  | Helen Zhang and Angela LansangXExperimental Design  | Will Hummel, Aditya Tummala, and Prasoon KhareXFermi Questions  | Samyoke and Serena An/TrisitnaXFossils  | Will Hummel and Aditya TummalaXHerpetology  | Helen Zhang and Angela LansangXProtein Modeling  | Marina Du, Jane Adry, and Joseph ParkXThermodynamics  | Grant Sternhagen *Can have two more peopleXWrite It Do It  | Bidhi Kasu and Serena An/Tristina TingXBoomilever (2) | Orange Team (Will, Aditya, Joshua, David, Joseph)XChemistry Lab | Marina Du and Jane AdryXForensics |David and Joseph ParkXGeological Mapping |Prasoon and JoshuaXMission Possible (2) |Everyone, if timeXMousetrap |Blue team (Grant, Samyok, Prasoon, Marina, Jane)XSounds of Music |David and Joseph ParkXWater Quality |Joshua ParkXWright Stuff | Green Team (Helen, Angela, Bidhi, Tristina, Carissa)X";
-    console.log(string.split("X"));
+    // console.log(string.split("X"));
     let newStrings = [];
-    string.split("X").forEach(item =>{
+    string.split("X").forEach((item, key) =>{
         if(!item){return 0;}
         let split = item.split("|");
         if(split[1]){
             // is taken
             newStrings.push({
-                event: split[0],
-                person: split[1],
+                event: ALL_EVENTS[key].title,
+                person: ALL_EVENTS[key].people.join(', '),
                 brookingsScore: 1,
                 yanktonScore: 2
             })
