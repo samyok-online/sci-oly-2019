b9ad9bc5e8fcec03f6ce077ac383115a0c78cb6e
Samyok Nepal <nepalsamyok@gmail.com>
2019-02-19 11:41:41 -0600

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -78,7 +78,7 @@ function startTimer() {
         var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
         var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-        document.getElementById("countdown").innerHTML = Math.floor(days / 7) + " weeks";
+        document.getElementById("countdown").innerHTML = days + " days left";
         if (distance < 0) {
             clearInterval(x);
             document.getElementById("demo").innerHTML = "Sci Oly 2019 has started!";
